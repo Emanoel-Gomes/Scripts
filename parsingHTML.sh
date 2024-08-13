88c110e27a6f62d0de2d4b177a69c7033c62441d
Emanoel Gomes <82549432+Emanoel-Gomes@users.noreply.github.com>
2024-08-13 11:04:13 -0300

    add parsingHTML

---
+++ parsingHTML.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# baixar html
+if [ "$1" == "" ] || [ "$2" == "" ]; then
+    echo "PARSING HTML"
+    echo "Modo de uso: $0 [site.com] [output_file]"
+else
+    wget -O site.html "$1"
+
+    # filtrar links
+    grep href site.html | cut -d "/" -f 3 | grep "\." | cut -d '"' -f 1 | grep -v "<l" > saida
+
+    # descobrir hosts
+    for url in $(cat saida); do 
+        host $url | grep "has address"
+    done > "$2"
+fi
